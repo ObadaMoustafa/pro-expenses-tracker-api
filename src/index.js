1636649026acb2d939313e03e9c59e1d08a47da5
Obada Moustafa Sharbatly <obadamoustafa@gmail.com>
2023-09-21 15:36:45 +0200

    made the test page shows text

--- src/index.js
+++ src/index.js
@@ -18,16 +18,16 @@ async function startServer() {
   }
 }
 
-if (process.env.NODE_ENV === "production") {
-  app.use(
-    express.static(new URL("../../client/build", import.meta.url).pathname)
-  );
-  // Redirect * requests to give the client data
-  app.get("*", (req, res) =>
-    res.sendFile(
-      new URL("../../client/build/index.html", import.meta.url).pathname
-    )
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(
+//     express.static(new URL("../../client/build", import.meta.url).pathname)
+//   );
+//   // Redirect * requests to give the client data
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       new URL("../../client/build/index.html", import.meta.url).pathname
+//     )
+//   );
+// }
 
 startServer();
--- src/routes/test.js
+++ src/routes/test.js
@@ -3,6 +3,7 @@ const testRouter = express.Router();
 
 testRouter.get("/", (req, res) => {
   console.log("server is connected successfully");
+  res.send("<h1>test page</h1>");
 });
 
 export default testRouter;
