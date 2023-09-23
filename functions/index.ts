dcda67b2269c6bd7440d7215ea76af5b6a19d2ba
Obada Moustafa Sharbatly <obadamoustafa@gmail.com>
2023-09-23 13:17:18 +0200

    functions folder out

---
+++ functions/index.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { app } from "../src/app.js";
+import { connectDB } from "../src/db/connectDB.js";
+import serverless from "serverless-http";
+// import express from "express";
+
+const port = process.env.PORT;
+if (port === null) {
+  throw new Error("port is null it seems you forgot to create env file");
+}
+
+async function startServer() {
+  try {
+    connectDB();
+    app.listen(port, () => console.log(`server is running in port ${port}`));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(
+//     express.static(new URL("../../client/build", import.meta.url).pathname)
+//   );
+//   // Redirect * requests to give the client data
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       new URL("../../client/build/index.html", import.meta.url).pathname
+//     )
+//   );
+// }
+export const handler = serverless(app);
+
+startServer();
