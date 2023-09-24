8e602d38d58fd63976945825359e20649c0d0c47
Obada Moustafa Sharbatly <obadamoustafa@gmail.com>
2023-09-24 14:04:43 +0200

    should work on netlify now

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
-import testRouter from "./routes/test.js";
-import usersRouter from "./routes/users.js";
-import expensesRouter from "./routes/expenses.js";
-import debtsRouter from "./routes/debts.js";
-
+import testRouter from "../../src/routes/test.js";
+import usersRouter from "../../src/routes/users.js";
+import expensesRouter from "../../src/routes/expenses.js";
+import debtsRouter from "../../src/routes/debts.js";
+import serverless from "serverless-http";
 export const app = express();
 
 // use json middleware
@@ -16,7 +16,10 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.json({ success: true, page: "root page" });
 });
+
 app.use("/api/test", testRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/expenses", expensesRouter);
 app.use("/api/debts", debtsRouter);
+
+export const handler = serverless(app);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { app } from "./app.js";
+import { app } from "../netlify/functions/app.js";
 import { connectDB } from "./db/connectDB.js";
-// import serverless from "serverless-http";
-// import express from "express";
 
 const port = process.env.PORT;
 if (!port) {
@@ -18,18 +16,5 @@ async function startServer() {
     console.error(error);
   }
 }
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(
-//     express.static(new URL("../../client/build", import.meta.url).pathname)
-//   );
-//   // Redirect * requests to give the client data
-//   app.get("*", (req, res) =>
-//     res.sendFile(
-//       new URL("../../client/build/index.html", import.meta.url).pathname
-//     )
-//   );
-// }
-// export const handler = serverless(app);
 
 startServer();
