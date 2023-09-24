487c02f5a4636ad7c462d88ea847b1e013287a9b
Obada Moustafa Sharbatly <obadamoustafa@gmail.com>
2023-09-24 14:21:02 +0200

    changed the name to api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { connectDB } from "./db/connectDB.js";
-import { app } from "../app.js";
+import { app } from "../api.js";
 
 const port = process.env.PORT;
 if (!port) {
